0c22453965f452cf96103a599ea9099ddee10825
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-06-07 16:14:51 -0400

    reload data nicely

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   scheduler.config.xml_date="%Y-%m-%d %H:%i";
   scheduler.config.start_on_monday = false;
   scheduler.config.show_loading = true;
+  scheduler.config.prevent_cache = true;
   /*
   scheduler.config.collision_limit = 1
   scheduler.attachEvent("onEventCollision", function (ev, evs){
@@ -15,8 +16,13 @@ $(document).ready(function() {
        return 'Subject: ' + ev.text + '';
   };
   scheduler.init('scheduler',new Date(),"month");
+
   scheduler.load("/events.json", 'json');
   var dp = new dataProcessor("/events/save");
   dp.init(scheduler);
   dp.setTransactionMode("POST",false);
+  dp.attachEvent("onAfterUpdateFinish",function(){
+    scheduler.clearAll();
+    scheduler.load("/events.json", 'json');
+  })
 });

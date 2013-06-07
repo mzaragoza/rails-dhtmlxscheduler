592656796adae07962dc456274e4ccd0392d636d
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-06-07 12:46:43 -0400

    except js

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module RailsDhtmlxscheduler
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.precompile += %w( calendar.js )
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
